ad8ff5e060465c83c26df71d97151c029d01122f
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 19:39:29 +0530

    test `mixUp`

--- test.ts
+++ test.ts
@@ -1,6 +1,4 @@
 import Colors from "./index";
 import Color from "./lib/Color";
 
-const color = new Color(Colors.iOS.Dark.Blue);
-
-console.log(color.mixUp(["#fff", "#fff", "#fff"], 20))
+console.log(Color.mixUp(["#fff", "#fff", "#fff"], 20))
